6affb8a33c08486144f386cc19546dceca0e65ac
Deamper <Deamper>
2017-02-18 14:48:17 +0200

    Initial commit

---
+++ src/Chislo.java
@@ -0,0 +1,10 @@
+
+public class Chislo {
+    public static void main(String[] args) {
+        int chislo = -356;
+        if (chislo > 0)
+            System.out.println(chislo+1);
+        else
+            System.out.println(chislo);
+    }
+}
